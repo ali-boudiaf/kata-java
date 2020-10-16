8144e42426f7be3a135246007d1444c59761be27
Ali Boudiaf <ali.boudiaf@insee.fr>
2020-10-16 08:25:09 +0200

    WIP

--- src/main/ToBinary.java
+++ src/main/ToBinary.java
@@ -5,19 +5,40 @@ import java.util.ArrayList;
 
 public class ToBinary {
 	public static int toBinary(int n) {
-		// Diviser le nombre par 2 (n modulo 2)
-		// Stocker le reste de la division dans un tableau
-		// Répéter jusqu'à 0 ou > 0
+		// Stocker le reste dans un tableau lorsque le nombre est par 2 (donc faire
+		// modulo 2)
+		// Divise le nombre par 2
+		// Répéter tant que nombre donnée > 0
 		// Inverser le tableau
 
-		ArrayList<Integer> tableauBinaire = new ArrayList<Integer>();
+		// Déclarer un tableau pour recevoir le reste du nombre modulo 2
+		int[] resteNombreModulo2 = new int[32];
 
-		for (int i = 0; i > 0; i++) {
-			Integer binaire = n % 2;
-			tableauBinaire.add(binaire);
-			System.out.println(tableauBinaire);
+		int i = 0;
+		while (n > 0) {
+
+			// Stocker le reste dans un tableau (modulo 2)
+			resteNombreModulo2[i] = n % 2;
+
+			// Diviser le nombre par 2
+			n = n / 2;
+			i++;
 		}
 
-		return n;
+		int resultat = 0;
+
+		// Inverser l'ordre du tableau
+		for (int j = i - 1; j >= 0; j--) {
+			StringBuilder tableauBinaireEnChaine = new StringBuilder();
+			tableauBinaireEnChaine.append(resteNombreModulo2[j]);
+			resultat = Integer.parseInt(tableauBinaireEnChaine.toString());
+		}
+		// System.out.println(resultat);
+		return resultat;
 	}
+
+	// public static void main(String[] args) {
+	// int n = 10;
+	// toBinary(n);
+	// }
 }

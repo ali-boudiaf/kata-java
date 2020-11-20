4cb6122be57f20e6f24fd6145f1e4b1d52e71fce
Ali Boudiaf <ali.boudiaf@insee.fr>
2020-11-20 11:00:43 +0100

    Un kata

---
+++ src/main/Positive.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Positive {
+	public static int sum(int[] arr){
+		int result = 0;
+		for (int i = 0; i < arr.length; i++) {
+			if (arr[i] > 0) {
+				result += arr[i];
+			}
+		}
+	    return result;
+	  }
+}

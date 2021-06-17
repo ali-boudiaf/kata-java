ebfe7f31d20eb1e486974a504415de2f7cd0caaf
Ali Boudiaf <ali.boudiaf@insee.fr>
2021-06-17 14:22:00 +0200

    kata Array plus array

---
+++ src/test/SumTest.java
@@ -0,0 +1,18 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import main.Sum;
+
+public class SumTest {
+
+	@Test
+    public void sampleTests() {
+        assertEquals(21, Sum.arrayPlusArray(new int[]{1,2,3}, new int[]{4,5,6}));
+        assertEquals(-21, Sum.arrayPlusArray(new int[]{-1,-2,-3}, new int[]{-4,-5,-6}));
+        assertEquals(15, Sum.arrayPlusArray(new int[]{0,0,0}, new int[]{4,5,6}));
+        assertEquals(2100, Sum.arrayPlusArray(new int[]{100,200,300}, new int[]{400,500,600}));
+    }
+}
